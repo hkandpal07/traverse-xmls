732fdae8eab77bc4267780735b24cc828b2dc917
Kandpal, Harshit <hkandpal@cvent.com>
2021-08-31 13:07:26 +0530

    Added logic to iterate on all xmls for data

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const axios = require('axios');
 const xmlReader = require('xml-reader');
 const xmlQuery = require('xml-query');
 
+const allHotelsObjects = {};
+
 const getData = async (url) => {
     try {
         const result = await axios.get(url, { timeout: 20000 })
         return result;
     } catch (err) {
-        throw err;
+        return err;
     }
 }
 
-getData('https://repos.accor.com/ota/content.xml').then(res => {
-        const hotelXML = xmlReader.parseSync(res.data);
+const getAllHotelData = async () => {
+    try {
+        const contentResponse = await getData('https://repos.accor.com/ota/content.xml');
+
+        const hotelXML = xmlReader.parseSync(contentResponse.data);
         const xq = xmlQuery(hotelXML);
 
         const hotelsNode = xq.find('hotels');
@@ -25,21 +30,54 @@ getData('https://repos.accor.com/ota/content.xml').then(res => {
             return xmlQuery(node).find('file').text();
         });
 
-        // console.log(allHotelXmls);
-
         const allXMLPaths = allHotelXmls.map(el => `https://repos.accor.com/ota/${el}`);
 
-        // console.log(allXMLPaths);
+        console.log(`Records to process: ${allXMLPaths.length}`)
 
-        getData(allXMLPaths[0]).then(data => {
-            const hotelData = data.data;
-            console.log(hotelData);
-        })
+        for (const [index, record] of allXMLPaths.entries()) {
+            if ((index+1)%10 === 0) {
+                console.log(index+1);
+            }
 
-        // allXMLPaths.forEach(element => {
-        //     const dataXml = await axios.get(element);
-        //     console.log(dataXML.data);
-        // });
+            const hotelXMLResponse = await getData(record);
 
-    }).catch(err => console.log(`Error: ${err}`));
+            if (hotelXMLResponse.status !== 200) {
+                console.log(`Couldn't get response for ${record}`);
+            } else {
+
+                const hotelDataXML = xmlReader.parseSync(hotelXMLResponse.data);
+                const hotelsXMLQuery = xmlQuery(hotelDataXML);
+
+                const hotelContentNode = hotelsXMLQuery.find('HotelDescriptiveContent');
+                const hotelCode = hotelContentNode.attr().HotelCode;
+                const meetingRoomsNode = hotelsXMLQuery.find('MeetingRooms');
+                const allMeetingRooms = meetingRoomsNode.children();
+
+                const currentMeetingRoomList = allMeetingRooms.map(node => {
+                    return {
+                        roomId: xmlQuery(node).attr('ID'),
+                        roomName: xmlQuery(node).attr('RoomName')
+                    }
+                })
+
+                const seen = new Set();
+                const currentMeetingRoomListFiltered = currentMeetingRoomList.filter(el => {
+                    const dup = seen.has(el.roomId);
+                    seen.add(el.roomId);
+                    return !dup;
+                })
+
+                allHotelsObjects[hotelCode] = currentMeetingRoomListFiltered;
+            }
+        }
+
+        // console.log(JSON.stringify(allHotelsObjects));
+
+        fs.writeFileSync('allData.json', JSON.stringify(allHotelsObjects));
+    } catch (err) {
+        console.log(`Fatal Error: ${err}`);
+    }
+}
+
+getAllHotelData();
 
